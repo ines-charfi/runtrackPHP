dd4553ac993a7a4837daf9fe6c38ec665a4f3a6d
Ines Jaffel <ines.charfi@laplateforme.io>
2025-01-23 15:50:57 +0100

    job12-13-11 ajouté

--- JOUR8-SQL/job18.sql
+++ JOUR8-SQL/job18.sql
@@ -1,1 +1,5 @@
-SELECT etage.nom AS Etage, salles.nom AS "Biggest Room", MAX(salles.capacite) FROM etage, salles ;
+
+SELECT etage.nom AS nom_etage, salles.nom AS `Biggest Room`, salles.capacite
+FROM salles
+JOIN etage ON salles.id_etage = etage.id
+WHERE salles.capacite = (SELECT MAX(capacite) FROM salles);
